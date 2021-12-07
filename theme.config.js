a63effea05a63ccd1a803296174f806dea51217b
Byeongjun Moon <byeongjun@bjmoon.io>
2021-12-06 23:53:34 -0800

    editing test

--- theme.config.js
+++ theme.config.js
@@ -1,10 +1,10 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/SantaDAO/docs',
+  docsRepositoryBase: 'https://github.com/SantaDAO/docs/blob/master',
+  titleSuffix: ' – SantaDAO',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">SantaDAO</span>
+      <span className="mr-2 font-extrabold hidden md:inline">🎅🏼 SantaDAO</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         A Manifesto
       </span>
@@ -16,13 +16,13 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Let's build Web3 culture on giving." />
+      <meta name="og:description" content="Let's build Web3 culture on giving." />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="Let's build Web3 culture on giving." />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>© {new Date().getFullYear()} SantaDAO</>,
   unstable_faviconGlyph: '👋',
 }
