a8fae1e287b003a0e70a4826565232f03fe577c8
Byeongjun Moon <byeongjun@bjmoon.io>
2021-12-18 17:18:00 -0800

    Changed info

--- theme.config.js
+++ theme.config.js
@@ -6,7 +6,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">🎄 elfDAO</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        A Manifesto
+        Documentation
       </span>
     </>
   ),
